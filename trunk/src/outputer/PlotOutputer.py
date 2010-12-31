deb8fc341fea418eb75577e59d2b438a099bf670
panpandas <panpandas@4d73ce26-f01c-a35a-b3e1-28a64a014117>
2010-12-31 12:22:56 +0000

    sort the input for plotOutputer so that plot model can plot line instead of just point
    git-svn-id: https://ultra-finance.googlecode.com/svn/trunk@33 4d73ce26-f01c-a35a-b3e1-28a64a014117

--- trunk/src/outputer/PlotOutputer.py
+++ trunk/src/outputer/PlotOutputer.py
@@ -6,6 +6,7 @@ Created on Dec 18, 2010
 from BaseModule import BaseModule
 from matplotlib import pyplot
 from datetime import datetime
+from operator import itemgetter
 import pylab
 
 class PlotOutputer(BaseModule):
@@ -16,11 +17,12 @@ class PlotOutputer(BaseModule):
         
     def execute(self, input):
         ''' do output '''
+        dateValues = sorted(input.items(), key=itemgetter(0))
         super(PlotOutputer, self).execute(input)
         fig = pylab.figure()
         ax = fig.gca()
  
         # Plotting here ...
-        ax.plot_date([datetime.strptime(date, '%Y-%m-%d') for date in input.keys()], input.values(), fmt='bo')
+        ax.plot_date([datetime.strptime(dateValue[0], '%Y-%m-%d') for dateValue in dateValues], \
+                     [dateValue[1] for dateValue in dateValues], fmt='b-')
         pyplot.show()
-        return None
