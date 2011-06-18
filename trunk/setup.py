3e68e1cfd97c669805ab1d40be2e5f1f11911256
panpandas <panpandas@4d73ce26-f01c-a35a-b3e1-28a64a014117>
2011-06-18 21:46:40 +0000

    add setup.py
    git-svn-id: https://ultra-finance.googlecode.com/svn/trunk@102 4d73ce26-f01c-a35a-b3e1-28a64a014117

---
+++ trunk/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0.0'
+
+setup(name='ultrafinance',
+      version=version,
+      description="python project for finance: realtime data collection, analyze, algorithmic trading",
+      long_description="""""",
+      classifiers=[
+        "Development Status :: 1 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+      ],
+      keywords='python, Finance, Algorithm, Trading, Realtime, QuantLib, pydispather',
+      author='Pan Pan',
+      author_email='panpandas@gmail.com',
+      url='http://code.google.com/p/ultra-finance/',
+      license='MIT',
+      packages=['ultrafinance', 'dataSource', 'doc', 'lib', 'test'],
+      include_package_data=True,
+      install_requires=[
+        'xlwt>=0.7.2',
+        'xlrd>=0.7.1',
+        'numpy>=1.5.1',
+        'scipy>=0.9.0rc3'
+      ]
+)
