5232f692cb0ad137f7c87b9a2d6d65984febe488
panpandas@gmail.com <panpandas@gmail.com@4d73ce26-f01c-a35a-b3e1-28a64a014117>
2011-10-11 06:39:26 +0000

    add pyTalib
    git-svn-id: https://ultra-finance.googlecode.com/svn/trunk@158 4d73ce26-f01c-a35a-b3e1-28a64a014117

---
+++ ultrafinance/pyTaLib/__init__.py
@@ -0,0 +1,33 @@
+''' ultraFinance TA-Lib implementation'''
+
+def sum():
+    ''' Summation '''
+    pass
+
+def min():
+    ''' Lowest value over a specified period '''
+    pass
+
+def max():
+    ''' Highest value over a specified period '''
+    pass
+
+def sma():
+    ''' Simple Moving Average '''
+    pass
+
+def ma():
+    ''' All Moving Average '''
+    pass
+
+def macd():
+    ''' Moving Average Convergence/Divergence '''
+    pass
+
+def stddev():
+    ''' Standard Deviation '''
+    pass
+
+def beta():
+    ''' beta '''
+    pass
